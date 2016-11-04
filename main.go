66aff5ee672721d03989e0a4c1c88aff4c6acfa4
tfenright <hailstormofnj@gmail.com>
2016-11-03 23:35:46 -0400

    rudimentary keyboard input for spacebar, at least

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/ebitenutil"
+        "fmt"
 )
 
 const (
@@ -10,15 +11,46 @@ const (
 	screenHeight = 400
 )
 
+type PlayerCharacter struct {
+        name string
+}
+
+var (
+        player *PlayerCharacter
+        // this is weird. used primarily to make sure we capture key presses rather than key holds
+        keyState = map[ebiten.Key]int{}
+)
+
+
+func (pc *PlayerCharacter) keyboardInput() error {
+
+
+        if !ebiten.IsKeyPressed(ebiten.KeySpace) {
+            keyState[ebiten.KeySpace] = 0
+                
+        } else {
+                keyState[ebiten.KeySpace]++
+                // when this is 1, it's just after we pushed a key, which means we should actually do something with it.
+                // should probably pull this out into its own thing, I think? we might be able to loop over all of the KeySpace
+                // and do it implicitly. not sure. 
+                if keyState[ebiten.KeySpace] == 1 {
+                        fmt.Print("you pushed space")
+                }
+        }
+        return nil
+}
+
 func update(screen *ebiten.Image) error {
 	p := &personImageParts{image: personImage}
 	screen.DrawImage(personImage, &ebiten.DrawImageOptions{
 		ImageParts: p,
 	})
 
+        player.keyboardInput()
 	ebitenutil.DebugPrint(screen, "Hello world!")
 	return nil
 }
+
 
 func main() {
 	var err error
@@ -27,5 +59,6 @@ func main() {
 		panic(err)
 	}
 
+        player = &PlayerCharacter{ name : "Test" }
 	ebiten.Run(update, screenWidth, screenHeight, 2, "Hello world!")
 }
