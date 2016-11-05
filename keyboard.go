d442088374c6ad239bf6b93c39b7a4ce323963f8
tfenright <hailstormofnj@gmail.com>
2016-11-04 22:19:04 -0400

    made keyboard work off of one list of constants rather than doing crazy math

--- keyboard.go
+++ keyboard.go
@@ -4,32 +4,83 @@ import "github.com/hajimehoshi/ebiten"
 
 type KeyboardWrapper struct {
 	keyState map[ebiten.Key]int
-	keyNames map[ebiten.Key]string
+	keys     []ebiten.Key
 }
 
 func NewKeyboardWrapper() *KeyboardWrapper {
 	var kw = &KeyboardWrapper{
 		keyState: map[ebiten.Key]int{},
-		keyNames: map[ebiten.Key]string{
-			ebiten.KeyBackspace: "BS",
-			ebiten.KeyComma:     ",",
-			ebiten.KeyDelete:    "Del",
-			ebiten.KeyEnter:     "Enter",
-			ebiten.KeyEscape:    "Esc",
-			ebiten.KeyPeriod:    ".",
-			ebiten.KeySpace:     "Space",
-			ebiten.KeyTab:       "Tab",
-
-			// Arrows
-			ebiten.KeyDown:  "Down",
-			ebiten.KeyLeft:  "Left",
-			ebiten.KeyRight: "Right",
-			ebiten.KeyUp:    "Up",
-
-			// Mods
-			ebiten.KeyShift:   "Shift",
-			ebiten.KeyControl: "Ctrl",
-			ebiten.KeyAlt:     "Alt",
+		keys: []ebiten.Key{
+			ebiten.Key0,
+			ebiten.Key1,
+			ebiten.Key2,
+			ebiten.Key3,
+			ebiten.Key4,
+			ebiten.Key5,
+			ebiten.Key6,
+			ebiten.Key7,
+			ebiten.Key8,
+			ebiten.Key9,
+			ebiten.KeyA,
+			ebiten.KeyB,
+			ebiten.KeyC,
+			ebiten.KeyD,
+			ebiten.KeyE,
+			ebiten.KeyF,
+			ebiten.KeyG,
+			ebiten.KeyH,
+			ebiten.KeyI,
+			ebiten.KeyJ,
+			ebiten.KeyK,
+			ebiten.KeyL,
+			ebiten.KeyM,
+			ebiten.KeyN,
+			ebiten.KeyO,
+			ebiten.KeyP,
+			ebiten.KeyQ,
+			ebiten.KeyR,
+			ebiten.KeyS,
+			ebiten.KeyT,
+			ebiten.KeyU,
+			ebiten.KeyV,
+			ebiten.KeyW,
+			ebiten.KeyX,
+			ebiten.KeyY,
+			ebiten.KeyZ,
+			ebiten.KeyAlt,
+			ebiten.KeyBackspace,
+			ebiten.KeyCapsLock,
+			ebiten.KeyComma,
+			ebiten.KeyControl,
+			ebiten.KeyDelete,
+			ebiten.KeyDown,
+			ebiten.KeyEnd,
+			ebiten.KeyEnter,
+			ebiten.KeyEscape,
+			ebiten.KeyF1,
+			ebiten.KeyF2,
+			ebiten.KeyF3,
+			ebiten.KeyF4,
+			ebiten.KeyF5,
+			ebiten.KeyF6,
+			ebiten.KeyF7,
+			ebiten.KeyF8,
+			ebiten.KeyF9,
+			ebiten.KeyF10,
+			ebiten.KeyF11,
+			ebiten.KeyF12,
+			ebiten.KeyHome,
+			ebiten.KeyInsert,
+			ebiten.KeyLeft,
+			ebiten.KeyPageDown,
+			ebiten.KeyPageUp,
+			ebiten.KeyPeriod,
+			ebiten.KeyRight,
+			ebiten.KeyShift,
+			ebiten.KeySpace,
+			ebiten.KeyTab,
+			ebiten.KeyUp,
+			ebiten.KeyMax,
 		},
 	}
 	return kw
@@ -44,28 +95,7 @@ func (kw *KeyboardWrapper) KeyPushed(key ebiten.Key) bool {
 }
 
 func (kw *KeyboardWrapper) Update() {
-	// A through Z
-	for c := 'A'; c <= 'Z'; c++ {
-		var key = ebiten.Key(c) - 'A' + ebiten.KeyA
-		if kw.IsKeyPressed(key) {
-			kw.keyState[key]++
-		} else {
-			kw.keyState[key] = 0
-		}
-	}
-
-	// numerics
-	for i := 1; i <= 12; i++ {
-		var key = ebiten.Key(i) + ebiten.KeyF1 - 1
-		if kw.IsKeyPressed(key) {
-			kw.keyState[key]++
-		} else {
-			kw.keyState[key] = 0
-		}
-	}
-
-	// and the weird ones
-	for key := range kw.keyNames {
+	for _, key := range kw.keys {
 		if kw.IsKeyPressed(key) {
 			kw.keyState[key]++
 		} else {
